542a6984be7a89f6982bc78365d5023be45b82bc
sumeet siranje <94678048+Sumeet-Siranje@users.noreply.github.com>
2022-03-04 09:34:47 +0000

    p1original.c_done

--- p1original.c
+++ p1original.c
@@ -1,30 +1,44 @@
-#include<stdio.h>
-void input(int *num1,int *den1,int *num2,int *den2){
-  printf("enter the first numerartor and denominator of fraction\n");
-  scanf("%d%d",num1,den1);
-  printf("enter the second numerartor and denominator of fraction\n");
-  scanf("%d%d",num2,den2);
-  
+#include <stdio.h>
+void input(int *num1 ,int *den1,int *num2,int *den2)
+{
+	printf("enter the numerator and denomenator of 1st fraction\n");
+	scanf("%d %d",num1,den1);
+	printf("enter the numerator and denomenator of 2nd fraction\n");
+	scanf("%d %d",num2,den2);
 }
-void add(int num1,int den1,int num2,int den2,int *num3,int *den3){
- 
-* num3=(den2*num1)+(den1*num2) ;
-  *den3=den1*den2;
-  for(int i=0;i<=num3 && i<=den3;++i){
-    if(num3%i==0 && den3%i==0)
-      return i;
-  }
-  
-  }
-void output(int num1, int den1, int num2, int den2, int num3, int den3){
-  
-  printf("the sum of %d/%d and %d/%d is %d/%d\n",num1,den1,num2,den2,num3,den3);
+void add(int num1,int den1,int num2,int den2,int *num3,int *den3)
+{
+	
+	  if(den1%den2==0)
+		{
+			*den3 = den1 ;
+			*num3 = num1 + ((num2)*(den1/den2));
+		}
+	else if(den2%den1==0)
+		{
+			*den3 = den2 ;
+			*num3 = num2 + ((num1)*(den2/den1));
+		}
+		else if(den1 = den2)
+		{
+			*den3 = den1;
+			*num3 = num1 + num2;
+		}
+	else
+	{
+			*den3 = (den1)*(den2);
+			*num3 = (num1)*(den2) + (num2)*(den1) ;
+	}
 }
-int main(){
-int p1,q1,p2,q2,p3,q3;
-  input(&p1,&q1,&p2,&q2);
-  add(p1,q1,p2,q2,&p3,&q3);
-  output(p1,q1,p2,q2,p3,q3);
-  return 0;
+void output(int num1,int den1,int num2,int den2,int num3,int den3)
+{
+	printf("%d/%d + %d/%d is %d/%d \n",num1,den1,num2,den2,num3,den3);
 }
-
+int main()
+{
+	int num1,num2,num3,den1,den2,den3;
+	input(&num1,&den1,&num2,&den2);
+	add(num1,den1,num2,den2,&num3,&den3);
+	output(num1,den1,num2,den2,num3,den3);
+	return 0;
+}
