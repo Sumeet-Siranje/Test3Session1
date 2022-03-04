2f42f2ec642d0c5da65bb22dc0f81fcbc5e33fe4
sumeet siranje <94678048+Sumeet-Siranje@users.noreply.github.com>
2022-03-04 10:34:48 +0000

    p2original.c_done

--- p1original.c
+++ p1original.c
@@ -25,7 +25,7 @@ void add(int num1,int den1,int num2,int den2,int *num3,int *den3)
 			*num3 = num1 + num2;
 		}
 	else
-	{
+	{,  
 			*den3 = (den1)*(den2);
 			*num3 = (num1)*(den2) + (num2)*(den1) ;
 	}
